13e75a7c3ef78c14a63359ae0299dc1e8856ce82
MatthiasSchenk <matthias.schenk91@gmail.com>
2015-09-29 09:56:43 +0200

    jkhjkh

--- RECOMMENDER/js/ResultWidget.js
+++ RECOMMENDER/js/ResultWidget.js
@@ -7,7 +7,6 @@ AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget.extend({
 	  console.log("sers");
 	  for (var i = 0, l = this.manager.response.response.docs.length; i < l; i++) {
 	    var doc = this.manager.response.response.docs[i];
-	    // $(this.target).append(this.template(doc));
 	    console.log(doc);
 
 	    //DATA
@@ -20,7 +19,6 @@ AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget.extend({
 	    var diabetus = doc.diabetus[0];
 	    var lactose = doc.lactose[0];
 
-	    console.log(""+alk+diabetus+lactose);
 
 	    //STRINGS
 	    	//duration
@@ -98,22 +96,7 @@ AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget.extend({
 
 
 
-	/** template: function (doc) {
-	  var snippet = '';
-	  if (doc.text.length > 300) {
-	    snippet += doc.dateline + ' ' + doc.text.substring(0, 300);
-	    snippet += '<span style="display:none;">' + doc.text.substring(300);
-	    snippet += '</span> <a href="#" class="more">more</a>';
-	  }
-	  else {
-	    snippet += doc.dateline + ' ' + doc.text;
-	  }
 
-	  var output = '<div><h2>' + doc.title + '</h2>';
-	  output += '<p id="links_' + doc.id + '" class="links"></p>';
-	  output += '<p>' + snippet + '</p></div>';
-	  return output;
-	} */
 
 });
 	var expandClickedRecipe = function(){
